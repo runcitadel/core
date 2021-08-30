e408d4f3a0ef35860cfa3e4c74a11fe33a7002c9
Aaron Dewes <aaron.dewes@web.de>
2021-08-30 16:02:52 +0100

    Add some notes

--- scripts/update/00-run.sh
+++ scripts/update/00-run.sh
@@ -6,6 +6,42 @@ UMBREL_ROOT=$2
 
 ./check-memory "${RELEASE}" "${UMBREL_ROOT}" "firstrun"
 
+# Functions which work like echo, but color the text red, green, and yellow
+# respectively.
+red() {
+    echo -e "\033[31m$@\033[0m"
+}
+green() {
+    echo -e "\033[32m$@\033[0m"
+}
+yellow() {
+    echo -e "\033[33m$@\033[0m"
+}
+
+echo -n "Running "; yellow "₿itcoin";
+sleep 5
+
+red "Please read the following notes carefully, as it explains details about bugs and other issues in the beta."
+sleep 7
+echo
+echo "Thanks for joining us on the Citadel beta, we're installing the update."
+sleep 3
+echo "There is an ongoing issue with the blocks not being displayed on the dashboard, please be patient, a fix is in development."
+sleep 3
+echo "Meanwhile, you can use the mempool app to check if the blocks are still being synced."
+sleep 3
+echo "Also, please do NOT install an update from the dashboard if it asks you to, or you might go back to Umbrel."
+sleep 3
+echo "We're sorry for all these issues, and beta 2 next week will fix most of them and include updated apps."
+sleep 3
+echo "If you have any questions, please DM us on Twitter."
+sleep 3
+echo "Waiting 60 seconds, if you want to cancel the update, turn your node off using the dashboard now"
+
+sleep 60
+echo
+green "Thanks for testing Citadel! The update will start now."
+
 echo
 echo "======================================="
 echo "=============== UPDATE ================"
