51c6f85c2613396c2146b90b131943e04befa416
Aaron Dewes <aaron.dewes@web.de>
2021-09-17 18:28:49 +0100

    Add all options from scripts/app to app-manager.py

--- app/app-manager.py
+++ app/app-manager.py
@@ -18,41 +18,57 @@ from sys import argv
 
 # Print an error if user is not root
 if os.getuid() != 0:
-  print('This script must be run as root!')
-  exit(1)
+    print('This script must be run as root!')
+    exit(1)
 
 # The directory with this script
 scriptDir = os.path.dirname(os.path.realpath(__file__))
 nodeRoot = os.path.join(scriptDir, "..")
 appsDir = os.path.join(nodeRoot, "apps")
+userFile = os.path.join(nodeRoot, "db", "user.json")
+legacyScript = os.path.join(nodeRoot, "scripts", "app")
 
 parser = argparse.ArgumentParser(description="Manage apps on your Citadel")
-parser.add_argument('action', help='What to do with the app database either list, download, update or update-online.')
-parser.add_argument('--invoked-by-configure', action='store_true')
+parser.add_argument('action', help='What to do with the app database.', choices=[
+                    "list", "download", "update", "update-online", "ls-installed", "install", "uninstall", "stop", "start", "compose"])
+# Add the --invoked-by-configure option, which is hidden from the user in --help
+parser.add_argument('--invoked-by-configure',
+                    action='store_true', help=argparse.SUPPRESS)
 parser.add_argument('--verbose', '-v', action='store_true')
+parser.add_argument(
+    'app', help='Optional, the app to perform an action on. (For install, uninstall, stop, start and compose)', nargs='?')
+parser.add_argument(
+    'other', help='Anything else (For compose)', nargs="*")
 args = parser.parse_args()
 
 # Returns a list of every argument after the second one in sys.argv joined into a string by spaces
+
+
 def getArguments():
     arguments = ""
     for i in range(3, len(argv)):
         arguments += argv[i] + " "
     return arguments
 
+
 # If no action is specified, the list action is used
 if args.action is None:
     args.action = 'list'
 
+
 def getAppYml(name):
-    url = 'https://raw.githubusercontent.com/runcitadel/compose-nonfree/main/apps/' + name + '/' + 'app.yml'
+    url = 'https://raw.githubusercontent.com/runcitadel/compose-nonfree/main/apps/' + \
+        name + '/' + 'app.yml'
     response = requests.get(url)
     if response.status_code == 200:
         return response.text
     else:
         return False
 
+
 def getAppYmlPath(app):
     return os.path.join(appsDir, app, 'app.yml')
+
 
 def composeToAppYml(app):
     composeFile = os.path.join(appsDir, app, "docker-compose.yml")
@@ -68,7 +84,6 @@ def composeToAppYml(app):
     # Put converted into the app.yml after encoding it as YAML
     with open(appYml, "w") as f:
         f.write(yaml.dump(converted, sort_keys=False))
-    
 
 
 def update():
@@ -83,7 +98,7 @@ def update():
     with open(os.path.join(appsDir, "apps.json"), "w") as f:
         json.dump(simpleRegistry, f, indent=4, sort_keys=True)
     print("Wrote version information to apps.json")
-    
+
     # Loop through the apps and generate valid compose files from them, then put these into the app dir
     for app in apps:
         composeFile = os.path.join(appsDir, app, "docker-compose.yml")
@@ -95,6 +110,7 @@ def update():
                 if args.verbose:
                     print("Wrote " + app + " to " + composeFile)
     print("Generated configuration successfully")
+
 
 def download():
     if(args.app is None):
@@ -115,6 +131,8 @@ def download():
             print("Warning: Could not download " + args.app)
 
 # Loads an app.yml and converts it to a docker-compose.yml
+
+
 def getApp(appFile: str, appId: str):
     with open(appFile, 'r') as f:
         app = yaml.safe_load(f)
@@ -122,11 +140,12 @@ def getApp(appFile: str, appId: str):
     if not "metadata" in app:
         raise Exception("Error: Could not find metadata in " + appFile)
     app["metadata"]["id"] = appId
-                
+
     if('version' in app and str(app['version']) == "1"):
         return createComposeConfigFromV1(app, nodeRoot)
     else:
         return createComposeConfigFromV0(app)
+
 
 def compose(app, arguments):
     # Runs a compose command in the app dir
@@ -138,8 +157,10 @@ def compose(app, arguments):
     # Save the previous working directory and return to it later
     oldDir = os.getcwd()
     os.chdir(os.path.join(nodeRoot, "apps", app))
-    os.system("docker compose --env-file '{}' {}".format(os.path.join(nodeRoot, ".env"), arguments))
+    os.system(
+        "docker compose --env-file '{}' {}".format(os.path.join(nodeRoot, ".env"), arguments))
     os.chdir(oldDir)
+
 
 if args.action == 'list':
     apps = findAndValidateApps(appsDir)
@@ -148,7 +169,7 @@ if args.action == 'list':
     exit(0)
 elif args.action == 'download':
     download()
-    exit(0)     
+    exit(0)
 elif args.action == 'update':
     if(args.invoked_by_configure):
         update()
@@ -172,11 +193,40 @@ elif args.action == 'update-online':
         os.system("docker compose stop app_2_tor")
         os.system("docker compose start app_2_tor")
     exit(0)
-#elif args.action == 'compose-to-app':
-    #apps = findApps(appsDir)
-    #for app in apps:
-        #composeToAppYml(app)
-    #exit(0)
+elif args.action == 'ls-installed':
+    # Load the userFile as JSON, check if installedApps is in it, and if so, print the apps
+    with open(userFile, "r") as f:
+        userData = json.load(f)
+    if "installedApps" in userData:
+        print("\n".join(userData["installedApps"]))
+    else:
+        # To match the behavior of the old script, print a newline if there are no apps installed
+        print("\n")
+elif args.action == 'install':
+    if not args.app:
+        print("No app provided")
+        exit(1)
+    os.system(legacyScript + " install " + args.app)
+elif args.action == 'uninstall':
+    if not args.app:
+        print("No app provided")
+        exit(1)
+    os.system(legacyScript + " uninstall " + args.app)
+elif args.action == 'stop':
+    if not args.app:
+        print("No app provided")
+        exit(1)
+    os.system(legacyScript + " stop " + args.app)
+elif args.action == 'start':
+    if not args.app:
+        print("No app provided")
+        exit(1)
+    os.system(legacyScript + " start " + args.app)
+elif args.action == 'compose':
+    if not args.app:
+        print("No app provided")
+        exit(1)
+    os.system(legacyScript + " compose " + args.app + " " + " ".join(args.other))
 else:
     print("Error: Unknown action")
     print("See --help for usage")
