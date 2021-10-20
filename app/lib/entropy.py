36d659b92f5c6fed5e8fa95cae745e0e3df659b2
Aaron Dewes <aaron.dewes@web.de>
2021-10-20 16:30:50 +0100

    Even more rebranding

--- app/lib/entropy.py
+++ app/lib/entropy.py
@@ -6,8 +6,8 @@ scriptDir = os.path.dirname(os.path.realpath(__file__))
 nodeRoot = os.path.join(scriptDir, "..", "..")
 
 def deriveEntropy(identifier: str):
-    seedFile = os.path.join(nodeRoot, "db", "umbrel-seed", "seed")
-    alternativeSeedFile = os.path.join(nodeRoot, "db", "umbrel-seed", "seed")
+    seedFile = os.path.join(nodeRoot, "db", "citadel-seed", "seed")
+    alternativeSeedFile = os.path.join(nodeRoot, "db", "citadel-seed", "seed")
     if not os.path.isfile(seedFile):
         if(os.path.isfile(alternativeSeedFile)):
             seedFile = alternativeSeedFile
