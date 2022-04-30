1e3556ac1f48433a66e1aa24e9624c4f36c541aa
AaronDewes <aaron.dewes@protonmail.com>
2022-04-30 11:02:19 +0000

    Fix for c-lightning

--- app/lib/composegenerator/shared/const.py
+++ app/lib/composegenerator/shared/const.py
@@ -39,7 +39,12 @@ def permissions():
                 "ELECTRUM_PORT",
             ],
             "volumes": []
-        }
+        },
+        "c-lightning": {
+            "environment_allow": [
+                "C_LIGHTNING_IP"
+            ],
+        },
     }
 
 # Vars which are always allowed without permissions
