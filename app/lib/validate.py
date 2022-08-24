61cbdb84785ba619606d1a4f69ce7627632e4a26
AaronDewes <aaron.dewes@protonmail.com>
2022-08-24 11:27:55 +0000

    Remove another unused argument

--- app/lib/validate.py
+++ app/lib/validate.py
@@ -84,7 +84,7 @@ def findAndValidateApps(dir: str):
         app_dir = subdir.path
         if os.path.isfile(os.path.join(app_dir, "app.yml.jinja")):
             os.chown(app_dir, 1000, 1000)
-            os.system("docker run --rm -v {}:/apps -u 1000:1000 {} /app-cli preprocess --app-name '{}' --port-map /apps/ports.json /apps/{}/app.yml.jinja /apps/{}/app.yml --services 'lnd'".format(dir, dependencies['app-cli'], subdir.name, subdir.name, subdir.name))
+            os.system("docker run --rm -v {}:/apps -u 1000:1000 {} /app-cli preprocess --app-name '{}' /apps/{}/app.yml.jinja /apps/{}/app.yml --services 'lnd'".format(dir, dependencies['app-cli'], subdir.name, subdir.name, subdir.name))
         if os.path.isfile(os.path.join(app_dir, "app.yml")):
             apps.append(subdir.name)
             # Read the app.yml and append it to app_data
