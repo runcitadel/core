9d98811761a79861c5a4f3e72f34d500f03281cf
AaronDewes <aaron.dewes@protonmail.com>
2022-09-09 07:58:33 +0000

    Fix merge

--- app/lib/manage.py
+++ app/lib/manage.py
@@ -35,30 +35,6 @@ from lib.metadata import getAppRegistry
 from lib.entropy import deriveEntropy
 from lib.citadelutils import FileLock
 
-class FileLock:
-    """Implements a file-based lock using flock(2).
-    The lock file is saved in directory dir with name lock_name.
-    dir is the current directory by default.
-    """
-
-    def __init__(self, lock_name, dir="."):
-        self.lock_file = open(os.path.join(dir, lock_name), "w")
-
-    def acquire(self, blocking=True):
-        """Acquire the lock.
-        If the lock is not already acquired, return None.  If the lock is
-        acquired and blocking is True, block until the lock is released.  If
-        the lock is acquired and blocking is False, raise an IOError.
-        """
-        ops = fcntl.LOCK_EX
-        if not blocking:
-            ops |= fcntl.LOCK_NB
-        fcntl.flock(self.lock_file, ops)
-
-    def release(self):
-        """Release the lock. Return None even if lock not currently acquired"""
-        fcntl.flock(self.lock_file, fcntl.LOCK_UN)
-
 # For an array of threads, join them and wait for them to finish
 def joinThreads(threads: List[threading.Thread]):
     for thread in threads:
