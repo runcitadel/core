769e98c60e554982f1b2e68fd1fd51b35798d33d
AaronDewes <aaron.dewes@protonmail.com>
2022-09-25 17:42:32 +0000

    Missing argument

--- app/lib/metadata.py
+++ app/lib/metadata.py
@@ -160,7 +160,7 @@ def getPortsV2App(app, appId):
 def getPortsV3App(app, appId):
     for appContainer in app["containers"]:
         containerAsDataClass = from_dict(data_class=Container, data=appContainer)
-        assignIp(appId)
+        assignIp(containerAsDataClass, appId)
         if "port" in appContainer:
             if "preferredOutsidePort" in appContainer and "requiresPort" in appContainer and appContainer["requiresPort"]:
                 validatePort(appContainer["name"], appContainer, appContainer["preferredOutsidePort"], appId, 2)
