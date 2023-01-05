26d4ff36dd3beab2f44a50df1bb8290be37c0099
AaronDewes <aaron.dewes@protonmail.com>
2023-01-05 17:43:25 +0000

    Fix entry name

--- app/app-manager.py
+++ app/app-manager.py
@@ -86,8 +86,8 @@ elif args.action == 'install':
     if registryEntry is None:
         print("App {} does not seem to exist".format(args.app))
         exit(1)
-    if isinstance(registryEntry.hidden_services, list):
-        for entry in registryEntry.hidden_services:
+    if isinstance(registryEntry.hiddenServices, list):
+        for entry in registryEntry.hiddenServices:
             if not os.path.exists(os.path.join(torDataDir, entry, "hostname")):
                 print("Restarting Tor containers...")
                 try:
